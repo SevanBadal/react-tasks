7c623ccddc7d8844a69ead2c8e5066704a9b7985
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-14 14:55:05 -0400

    img removed

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ const Header = (props) => (
     <div className="container">
       <div className="header__title-container">
         <h1 className="header__title">{props.title}</h1>
-        <img src={img} alt="react icon" height="42" width="42" />
       </div>
       {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
     </div>
